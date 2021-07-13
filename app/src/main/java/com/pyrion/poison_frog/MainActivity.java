16574378fa20f87c19c939531e458503f9d5c725
pyrio <hanescargot@gamil.com>
2021-07-13 20:34:42 +0900

    2021-07-13 pyrion

--- app/src/main/java/com/pyrion/poison_frog/MainActivity.java
+++ app/src/main/java/com/pyrion/poison_frog/MainActivity.java
@@ -6,14 +6,20 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
+
+import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText foodInput;
     TextView foodLog;
-    String foodInputSting;
+    String foodInputSting = "";
+    ScrollView foodLogScroll;
+    Stack<String> originFoodName = new Stack<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,21 +28,31 @@ public class MainActivity extends AppCompatActivity {
 
         foodInput = findViewById(R.id.food_name_input);
         foodLog = findViewById(R.id.food_log);
+        foodLogScroll = findViewById(R.id.log_scroll);
 
-        TextView.OnEditorActionListener foodInputActionListener = new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                System.out.println(event);
-                return false;
-            }
-        };
-
-        foodInput.addTextChangedListener((TextWatcher) foodInputActionListener);
-
+        foodInput.setOnEditorActionListener(foodInputActionListener);
 
 
     }
 
 
+    TextView.OnEditorActionListener foodInputActionListener = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            if(event == null){
+                foodInputSting += v.getText().toString() + "\n";
+                originFoodName.add(v.getText().toString());
+                ////
 
+                foodInputSting += "옴뇸뇸뇸 맛있는"+originFoodName.pop()+"네요." + "\n";
+
+                ///
+                foodLog.setText(foodInputSting);
+                foodLogScroll.fullScroll(View.FOCUS_DOWN);
+                v.setText("");
+
+            }
+            return false;
+        }
+    };
 }
