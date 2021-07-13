10c793e3a5a2fdcf79dff3e2118154de4ced4629
hanescargot <hanescargot@gmail.com>
2021-07-13 17:50:02 +0900

    5:50 mrhi5:50 mrhi5:50 mrhi5:50 mrhi5:50 mrhi

--- app/src/main/java/com/pyrion/poison_frog/MainActivity.java
+++ app/src/main/java/com/pyrion/poison_frog/MainActivity.java
@@ -3,12 +3,40 @@ package com.pyrion.poison_frog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText foodInput;
+    TextView foodLog;
+    String foodInputSting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        foodInput = findViewById(R.id.food_name_input);
+        foodLog = findViewById(R.id.food_log);
+
+        TextView.OnEditorActionListener foodInputActionListener = new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                System.out.println(event);
+                return false;
+            }
+        };
+
+        foodInput.addTextChangedListener((TextWatcher) foodInputActionListener);
+
+
+
     }
+
+
+
 }
